1f6d0a4816aa38f3f18d52166ade2536c5322b5f
PinJui <71209752+PinJui@users.noreply.github.com>
2020-11-24 00:14:16 +0800

    Update inference2txt.py

--- inference2txt.py
+++ inference2txt.py
@@ -6,8 +6,9 @@ checkpoint = 'work_dirs/faster_rcnn_r50_fpn_1x_coco/latest.pth'
 device = 'cuda:0'
 dataroot = '/home/aimmlab/Documents/hw2/mmdetection/data/VOCdevkit/VOC2007/test/'
 model = init_detector(config, checkpoint, device)
-with open('work_dirs/result_output10epochhw2.txt','w') as f:
-    for i in range(1,13069):
+with open('work_dirs/result_output10epochhw2.txt', 'w') as f:
+    for i in range(1, 13069):
         result = inference_detector(model, dataroot+str(i)+'.png')
         for k in result:
             f.write(str(k.tolist())+'\n')
+
